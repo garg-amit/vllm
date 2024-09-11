e93c53d0e74b1d1dfe0410838bb08094739c0ac2
bnellnm <49004751+bnellnm@users.noreply.github.com>
2024-09-11 15:52:19 -0400

    [Kernel][Misc] register ops to prevent graph breaks (#6917)
    Co-authored-by: Sage Moore <sage@neuralmagic.com>
    Signed-off-by: Amit Garg <mitgarg17495@gmail.com>

--- csrc/torch_bindings.cpp
+++ csrc/torch_bindings.cpp
@@ -74,22 +74,11 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
 
   // prepare_inputs advance_step
   ops.def(
-      "advance_step_flashattn(int num_seqs, int num_queries, int block_size, "
+      "advance_step(int num_seqs, int num_queries, int block_size, "
       "Tensor! input_tokens, Tensor sampled_token_ids, "
       "Tensor! input_positions, Tensor! seq_lens, Tensor! slot_mapping, "
       "Tensor block_tables) -> ()");
-  ops.impl("advance_step_flashattn", torch::kCUDA, &advance_step_flashattn);
-
-  ops.def(
-      "advance_step_flashinfer("
-      "    int num_seqs, int num_queries, int block_size,"
-      "    Tensor! input_tokens, Tensor sampled_token_ids,"
-      "    Tensor! input_positions, Tensor! seq_lens, Tensor! slot_mapping,"
-      "    Tensor block_tables, Tensor! paged_kv_indices,"
-      "    Tensor! paged_kv_indptr, Tensor! paged_kv_last_page_len,"
-      "    Tensor! block_table_bounds"
-      ") -> ()");
-  ops.impl("advance_step_flashinfer", torch::kCUDA, &advance_step_flashinfer);
+  ops.impl("advance_step", torch::kCUDA, &advance_step);
 
   // Layernorm
   // Apply Root Mean Square (RMS) Normalization to the input tensor.
